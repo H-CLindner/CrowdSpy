55d1fdb3de043ab78a63d757ce0d7174a89637a6
Hans-Christian Lindner <Hans-Christian Lindner>
2017-06-09 20:28:50 +0200

    canvas with arrows which are draggable

--- app/directives/drawing.js
+++ app/directives/drawing.js
@@ -12,40 +12,42 @@
                 template: '<canvas id="canvas" height="300" width="300" style="display:block; margin:1em auto; border:1px solid black; background-size: 100% 100%;"></canvas>',
                 link: function (scope, element, attrs) {
 
-                    var c = document.getElementById('canvas');
-                    var ctx = c.getContext("2d");
+                        var c = document.getElementById('canvas');
+                        var ctx = c.getContext("2d");
+                        dragging();
 
-                    function initialise(){
-                        var image = new Image();
-                        image.src = scope.plus;
-                        ctx.drawImage(image, 0, 0, 300, 300);
-                    }
-
-                    function drawArrow(fromx, fromy, tox, toy) {
+                        function Arrow(fromx, fromy, tox, toy) {
                             //variables to be used when creating the arrow
-                            var headlen = 1;
+                            this.x1 = fromx;
+                            this.y1 = fromy;
+                            this.x2 = tox;
+                            this.y2 = toy;
+                            this.headlen = 1;
+                        }
 
-                            var angle = Math.atan2(toy - fromy, tox - fromx);
+                        Arrow.prototype.drawToContext = function (context) {
+
+                            var angle = Math.atan2(this.y2 - this.y1, this.x2 - this.x1);
 
                             //starting path of the arrow from the start square to the end square and drawing the stroke
                             ctx.beginPath();
-                            ctx.moveTo(fromx, fromy);
-                            ctx.lineTo(tox, toy);
+                            ctx.moveTo(this.x1, this.y1);
+                            ctx.lineTo(this.x2, this.y2);
                             ctx.strokeStyle = "#cc0000";
                             ctx.lineWidth = 3;
                             ctx.stroke();
 
                             //starting a new path from the head of the arrow to one of the sides of the point
                             ctx.beginPath();
-                            ctx.moveTo(tox, toy);
-                            ctx.lineTo(tox - headlen * Math.cos(angle - Math.PI / 7), toy - headlen * Math.sin(angle - Math.PI / 7));
+                            ctx.moveTo(this.x2, this.y2);
+                            ctx.lineTo(this.x2 - this.headlen * Math.cos(angle - Math.PI / 7), this.y2 - this.headlen * Math.sin(angle - Math.PI / 7));
 
                             //path from the side point of the arrow, to the other side point
-                            ctx.lineTo(tox - headlen * Math.cos(angle + Math.PI / 7), toy - headlen * Math.sin(angle + Math.PI / 7));
+                            ctx.lineTo(this.x2 - this.headlen * Math.cos(angle + Math.PI / 7), this.y2 - this.headlen * Math.sin(angle + Math.PI / 7));
 
                             //path from the side point back to the tip of the arrow, and then again to the opposite side point
-                            ctx.lineTo(tox, toy);
-                            ctx.lineTo(tox - headlen * Math.cos(angle - Math.PI / 7), toy - headlen * Math.sin(angle - Math.PI / 7));
+                            ctx.lineTo(this.x2, this.y2);
+                            ctx.lineTo(this.x2 - this.headlen * Math.cos(angle - Math.PI / 7), this.y2 - this.headlen * Math.sin(angle - Math.PI / 7));
 
                             //draws the paths created above
                             ctx.strokeStyle = "#cc0000";
@@ -53,13 +55,161 @@
                             ctx.stroke();
                             ctx.fillStyle = "#cc0000";
                             ctx.fill();
+                        };
+
+                        Arrow.prototype.hitTest = function (hitX, hitY) {
+                            var hit1 = ((hitX > this.x1) && (hitX < this.x2) || (hitX > this.x2) && (hitX < this.x1));
+                            var hit2 = ((hitY > this.y1) && (hitY < this.y2) || (hitY > this.y2) && (hitY < this.y1));
+                            return (hit1 && hit2);
+                        };
+
+                        function dragging() {
+
+                            var shapes = [];
+                            var i;
+                            var mouseX;
+                            var mouseY;
+                            var dragIndex;
+                            var dragHoldX;
+                            var dragHoldY;
+                            var dragHoldX2;
+                            var dragHoldY2;
+                            var dragging;
+                            var targetX;
+                            var targetY;
+                            var targetX2;
+                            var targetY2;
+                            var timer;
+                            var easeAmount = 0.5;
+
+                            initialise();
+
+                            c.addEventListener("mousedown", mouseDownListener, false);
+
+
+                            var arrow1 = new Arrow(100, 100, 50, 50);
+                            var arrow2 = new Arrow(200, 200, 150, 150);
+                            var arrow3 = new Arrow(300, 300, 250, 250);
+                            var arrow4 = new Arrow(150, 200, 100, 150);
+                            var arrow5 = new Arrow(200, 250, 150, 200);
+                            console.log(arrow1);
+                            shapes.push(arrow1, arrow2, arrow3, arrow4, arrow5);
+
+                            function mouseDownListener(evt) {
+
+                                var bRect = c.getBoundingClientRect();
+                                mouseX = (evt.clientX - bRect.left) * (c.width / bRect.width);
+                                mouseY = (evt.clientY - bRect.top) * (c.height / bRect.height);
+
+                                for (i = 0; i < shapes.length; i++) {
+                                    if (shapes[i].hitTest(mouseX, mouseY)) {
+                                        dragging = true;
+                                        dragIndex = i;
+                                    }
+                                }
+
+                                if (dragging) {
+                                    window.addEventListener("mousemove", mouseMoveListener, false);
+
+                                    shapes.push(shapes.splice(dragIndex, 1)[0]);
+
+                                    dragHoldX = mouseX - shapes[shapes.length - 1].x1;
+                                    dragHoldY = mouseY - shapes[shapes.length - 1].y1;
+                                    dragHoldX2 = mouseX - shapes[shapes.length - 1].x2;
+                                    dragHoldY2 = mouseY - shapes[shapes.length - 1].y2;
+
+
+                                    targetX = mouseX - dragHoldX;
+                                    targetY = mouseY - dragHoldY;
+                                    targetX2 = mouseX - dragHoldX2;
+                                    targetY2 = mouseY - dragHoldY2;
+
+                                    timer = setInterval(onTimerTick, 1000 / 30);
+                                }
+
+                                c.removeEventListener("mousedown", mouseDownListener, false);
+                                window.addEventListener("mouseup", mouseUpListener, false);
+
+                                if (evt.preventDefault) {
+                                    evt.preventDefault();
+                                } //standard
+                                else if (evt.returnValue) {
+                                    evt.returnValue = false;
+                                } //older IE
+                                return false;
+                            }
+
+                            function onTimerTick() {
+                                shapes[shapes.length - 1].x1 = shapes[shapes.length - 1].x1 + easeAmount * (targetX - shapes[shapes.length - 1].x1);
+                                shapes[shapes.length - 1].y1 = shapes[shapes.length - 1].y1 + easeAmount * (targetY - shapes[shapes.length - 1].y1);
+                                shapes[shapes.length - 1].x2 = shapes[shapes.length - 1].x2 + easeAmount * (targetX2 - shapes[shapes.length - 1].x2);
+                                shapes[shapes.length - 1].y2 = shapes[shapes.length - 1].y2 + easeAmount * (targetY2 - shapes[shapes.length - 1].y2);
+
+                                if ((!dragging) && (Math.abs(shapes[shapes.length - 1].x1 - targetX) < 0.1) && (Math.abs(shapes[shapes.length - 1].y1 - targetY) < 0.1) && (Math.abs(shapes[shapes.length - 1].x2 - targetX2) < 0.1) && (Math.abs(shapes[shapes.length - 1].y2 - targetY2) < 0.1)) {
+                                    shapes[shapes.length - 1].x1 = targetX;
+                                    shapes[shapes.length - 1].y1 = targetY;
+                                    shapes[shapes.length - 1].x2 = targetX2;
+                                    shapes[shapes.length - 1].y2 = targetY2;
+                                    //stop timer:
+                                    clearInterval(timer);
+                                }
+                                initialise();
+                            }
+
+                            function mouseUpListener(evt) {
+                                c.addEventListener("mousedown", mouseDownListener, false);
+                                window.removeEventListener("mouseup", mouseUpListener, false);
+                                if (dragging) {
+                                    dragging = false;
+                                    window.removeEventListener("mousemove", mouseMoveListener, false);
+                                }
+                            }
+
+                            function mouseMoveListener(evt) {
+                                var posX;
+                                var posY;
+                                var posX2;
+                                var posY2;
+                                var minX = 2;
+                                var maxX = c.width - 2;
+                                var minY = 2;
+                                var maxY = c.height - 2;
+
+                                //getting mouse position correctly
+                                var bRect = c.getBoundingClientRect();
+                                mouseX = (evt.clientX - bRect.left) * (c.width / bRect.width);
+                                mouseY = (evt.clientY - bRect.top) * (c.height / bRect.height);
+
+                                //clamp x and y positions to prevent object from dragging outside of canvas
+                                posX = mouseX - dragHoldX;
+                                posX2 = mouseX - dragHoldX2;
+                                posX = (posX < minX) ? minX : ((posX > maxX) ? maxX : posX);
+                                posX2 = (posX2 < minX) ? minX : ((posX2 > maxX) ? maxX : posX2);
+                                posY = mouseY - dragHoldY;
+                                posY2 = mouseY - dragHoldY2;
+                                posY = (posY < minY) ? minY : ((posY > maxY) ? maxY : posY);
+                                posY2 = (posY2 < minY) ? minY : ((posY2 > maxY) ? maxY : posY2);
+
+                                targetX = posX;
+                                targetY = posY;
+                                targetX2 = posX2;
+                                targetY2 = posY2;
+                            }
+
+                            function drawShapes() {
+                                for (i = 0; i < shapes.length; i++) {
+                                    shapes[i].drawToContext(ctx);
+                                }
+                            }
+
+                            function initialise() {
+                                var image = new Image();
+                                image.src = scope.plus;
+                                ctx.drawImage(image, 0, 0, 300, 300);
+                                drawShapes();
+                            }
+
                         }
-                        initialise();
-                        drawArrow(100, 100, 50, 50);
-                        drawArrow(200, 200, 150, 150);
-                        drawArrow(300, 300, 250, 250);
-                        drawArrow(150, 0, 150, 50);
-                        drawArrow(200, 0, 200, 50);
                 }
             }
         });
